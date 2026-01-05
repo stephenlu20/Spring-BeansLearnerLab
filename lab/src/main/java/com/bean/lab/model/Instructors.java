f359b3e7e885999ad434527fd53f7134a2c03516
Stephen <stephen.lu20@gmail.com>
2026-01-05 12:04:40 -0500

    Added instructors model

---
+++ lab/src/main/java/com/bean/lab/model/Instructors.java
@@ -0,0 +1,9 @@
+package com.bean.lab.model;
+
+import java.util.List;
+
+public class Instructors extends People<Instructor> {
+    public Instructors(List<Instructor> instructors) {
+        super(instructors);
+    }
+}
